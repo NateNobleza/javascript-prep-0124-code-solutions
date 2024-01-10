b5c2781726b1dce3161a4030803b9e2c15ed403a
Nate Nobleza <noblezanathan@gmail.com>
2024-01-10 05:03:43 +0000

    completed prep-javascript-operators-and-expressions

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,32 @@
+const number1 = 21
+const number2 = 22
+const product = number1 * number2
+console.log(product)
+console.log(typeof product)
+const charge = 47
+const payment = 28
+const amountRemaining = payment - charge
+console.log(amountRemaining)
+console.log(typeof amountRemaining)
+const tests = 3
+const assignments = 4
+const final = 5
+const grade = ((tests + assignments + final) / (3) )
+console.log(grade)
+console.log(typeof grade)
+const firstName = 'Nathaniel';
+const lastName = 'Nobleza';
+const fullName = (firstName + " " + lastName)
+console.log(fullName)
+console.log(typeof fullName)
+const pH = 7
+const isAcidic = 4 < 7
+console.log(isAcidic)
+console.log(typeof isAcidic)
+const numberOfSoldiers = 333
+const isSparta = 'numberOfSoldiers' === 300
+console.log(isSparta)
+console.log(typeof isSparta)
+let nickName = fullName +" " + 'is the Goat'
+console.log(nickName)
+console.log(typeof nickName)
