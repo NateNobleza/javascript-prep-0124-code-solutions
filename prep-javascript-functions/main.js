1f8327f73f5a699695a36c2703c7c9e42844d7f9
Nate Nobleza <noblezanathan@gmail.com>
2024-01-13 09:02:29 +0000

    prep-javascript-prep-functions

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,68 @@
+function addTwoNumbers(num1, num2){
+
+  return num1 + num2;
+}
+const addTwoNumbersResult = addTwoNumbers(6, 6);
+console.log(addTwoNumbersResult);
+
+
+function convertHoursToMinutes(hours){
+const minutes = hours* 60;
+return minutes;
+}
+const convertHoursToMinutesResults = convertHoursToMinutes(19);
+console.log(convertHoursToMinutesResults);
+
+
+function getGreeting(name){
+const hello = 'Hi' + ' ' + 'there';
+return hello;
+}
+const getGreetingResult = getGreeting(name);
+console.log(getGreetingResult);
+
+
+
+function addAndMultiplyBy5(num1, num2){
+return (num1 + num2) * 5;
+}
+const addAndMultiplyBy5Result = addAndMultiplyBy5(310, 424);
+console.log(addAndMultiplyBy5Result);
+
+
+function multiplyAndDivideBy5(num1, num2){
+return (num1 * num2) / 5;
+}
+const multiplyAndDivideBy5Result = multiplyAndDivideBy5(310, 424);
+console.log(multiplyAndDivideBy5Result);
+
+
+function subtractTwoNumbers(num1, num2){
+  return num1 - num2
+}
+const subtractTwoNumbersResult = subtractTwoNumbers(310, 424)
+console.log(subtractTwoNumbersResult)
+
+
+function getCircleCircumfrence(radius){
+  const circumfrence = radius* (2 * 3.14);
+  return circumfrence;
+}
+const getCircleCircumfrenceResult = getCircleCircumfrence(5);
+console.log(getCircleCircumfrenceResult);
+
+
+function getFullName(firstName, lastName){
+const fullName = firstName + lastName;
+  return fullName;
+}
+var fullName = 'Nathaniel Nobleza';
+console.log(fullName);
+
+
+function cube(number){
+const formula = number * number * number
+return formula
+}
+const cubeResult = cube(5)
+console.log(cubeResult)
---
+++ prep-javascript-functions/prep-javascript-functions-quiz-notes.md
@@ -0,0 +1,41 @@
+# prep-javascript-functions-quiz-notes
+
+## Quiz Questions
+
+Answer the following questions in the provided markdown file before turning in this exercise:
+
+- What are the five parts of a function definition?
+Defining, Calling, Retuning, Naming, Anonymous
+- How do you call a function?
+by placing () in front of it, it causes the code block with in the definition to be executed
+- What is the difference between a parameter and an argument?
+the parameter is the place holder of the argument, parameter is a variable with an unknown value until we call the function and pass the argument
+ie; there is an argument to hello
+hello(there)
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
